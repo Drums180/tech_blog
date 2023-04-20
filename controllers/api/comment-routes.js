b7935114106ac2547d1a112b56a08d97c7c9f81d
Drums180 <daviddomor180@hotmail.com>
2023-04-20 13:58:35 -0600

    comment changes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Comment } = require("../../models");
+const { Comment, User } = require("../../models");
 
 // Get all comments
 router.get("/", async (req, res) => {
@@ -22,14 +22,21 @@ router.post("/", async (req, res) => {
   }
 
   try {
+    console.log("Request body:", req.body); // Add this line to log the request body
     const comment = await Comment.create({
       ...req.body,
       author_id: req.session.user_id,
     });
-    res.json(comment);
+
+    // Fetch the comment with the associated user information
+    const commentWithUser = await Comment.findByPk(comment.id, {
+      include: { model: User, attributes: ["username"] },
+    });
+
+    res.json(commentWithUser);
   } catch (err) {
-    console.error(err);
-    res.status(400).json(err);
+    console.error("Error details:", err); // Modify this line to log the error details
+    res.status(400).json({ error: err.message });
   }
 });
 
