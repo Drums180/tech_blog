5e4b862c2496669b0badf009fb3af510322cb40c
Drums180 <daviddomor180@hotmail.com>
2023-04-20 18:30:09 -0600

    final comment changes

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -4,26 +4,26 @@ const commentData = [
   {
     date_created: "2023-04-15 21:17:19",
     content: "Comment 1",
-    post_id: 1,
-    author_id: 1,
+    post_id: 4,
+    author_id: 4,
   },
   {
     date_created: "2023-04-15 21:17:19",
     content: "Comment 2",
-    post_id: 1,
-    author_id: 2,
+    post_id: 4,
+    author_id: 14,
   },
   {
     date_created: "2023-04-15 21:17:19",
     content: "Comment 3",
-    post_id: 2,
-    author_id: 3,
+    post_id: 14,
+    author_id: 24,
   },
   {
     date_created: "2023-04-15 21:17:19",
     content: "Comment 4",
-    post_id: 2,
-    author_id: 1,
+    post_id: 14,
+    author_id: 4,
   },
 ];
 
