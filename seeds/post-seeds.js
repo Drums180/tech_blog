ce46d3c1cfa914a7ea4b99e818298b113e9c382d
Drums180 <daviddomor180@hotmail.com>
2023-04-20 18:27:32 -0600

    seed changes

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -6,19 +6,19 @@ const postData = [
   {
     title: "Post 1",
     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    author_id: 1,
+    author_id: 4,
   },
   {
     title: "Post 2",
     content:
       "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    author_id: 2,
+    author_id: 14,
   },
   {
     title: "Post 3",
     content:
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    author_id: 1,
+    author_id: 4,
   },
 ];
 
